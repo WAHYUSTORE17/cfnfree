16ecddf4d4056eba9cf87c08686eec0322c36d61
WAHYUSTORE17 <142437082+WAHYUSTORE17@users.noreply.github.com>
2026-05-10 23:50:56 +0700

    Update about.sh

--- ssh/about.sh
+++ ssh/about.sh
@@ -2,13 +2,13 @@
 
 clear
 echo -e "================================================="
-echo -e "#     Premium Auto Script By Akbar Maulana      #"
+echo -e "#     Premium Auto Script By Wahyu Store      #"
 echo -e "================================================="
 echo -e "# For Debian 10 64 bit                          #"
 echo -e "# For Ubuntu 18.04 & Ubuntu 20.04 64 bit        #"
 echo -e "# For VPS with KVM and VMWare virtualization    #"
-echo -e "# Build Up By Akbar Maulana                     #"
-echo -e "# MOD By LIZSVR                                 #"
+echo -e "# Build Up By Wahyu Store                     #"
+echo -e "# MOD By Wahyu Store                                 #"
 echo -e "================================================="
 echo -e "# Thanks To                                     #"
 echo -e "================================================="
