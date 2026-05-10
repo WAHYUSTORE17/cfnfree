147f8fbf1d16a4b1cad27fa02fb5f8a6edb4d9eb
WAHYUSTORE17 <142437082+WAHYUSTORE17@users.noreply.github.com>
2026-05-11 00:06:03 +0700

    Update cfnhost.sh

--- ssh/cfnhost.sh
+++ ssh/cfnhost.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# My Telegram : https://t.me/Akbar218
+# My Telegram : https://t.me/WahyuProject17
 # ==========================================
 # Color
 RED='\033[0;31m'
@@ -18,13 +18,13 @@ success="${GREEN}[SUCCESS]${NC}"
 # Getting
 MYIP=$(wget -qO- ipinfo.io/ip);
 echo "Checking VPS"
-IZIN=$(curl https://raw.githubusercontent.com/lizsvr/cfnfree/main/ipvps.txt | grep $MYIP | awk '{print $3}')
+IZIN=$(curl https://raw.githubusercontent.com/WAHYUSTORE17/cfnfree/main/ipvps.txt | grep $MYIP | awk '{print $3}')
 if [ $MYIP = $MYIP ]; then
 echo -e "${NC}${GREEN}Permission Accepted...${NC}"
 else
 echo -e "${NC}${RED}Permission Denied!${NC}";
 echo -e "${NC}${LIGHT}Please Contact Admin!!"
-echo -e "${NC}${LIGHT}Telegram : https://t.me/liz_mine"
+echo -e "${NC}${LIGHT}Telegram : https://t.me/WahyuProject17"
 exit 0
 fi
 clear
